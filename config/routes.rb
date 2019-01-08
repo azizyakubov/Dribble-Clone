7c940c306750999856a52e18cb6e7fcd5140e58b
Aziz Yakubov <azizy15@gmail.com>
2019-01-08 13:07:54 -0800

    scaffolded shots controller and added relations between shots and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  resources :shots
+
   devise_for :users, controllers: { registrations: 'registrations' }
-  root 'home#index'
+
+  root 'shots#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190108205656) do
+ActiveRecord::Schema.define(version: 20190108210058) do
+
+  create_table "shots", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
