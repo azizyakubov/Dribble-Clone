9274ebec21a4bcbb70443069582ed0498155dcef
Aziz Yakubov <azizy15@gmail.com>
2019-01-24 17:42:52 -0800

    implemented shots controller with all the views and drag/drop func

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190108210058) do
+ActiveRecord::Schema.define(version: 20190124233201) do
 
   create_table "shots", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20190108210058) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_shot"
   end
 
   create_table "users", force: :cascade do |t|
